0338ec5453315fdd4ebfbd092fa057d6b2289c57
Daryllwrld <bonzodaryll0@gmail.com>
2026-05-06 08:57:26 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,41 +2,33 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-# THE FACEBOOK MASTER DATA
-# Everything here is dedicated to your FB Page
-fb_stats = {
+# THE META-ENGINE DATA
+# This represents the "Boost" happening on the actual FB servers
+fb_engine = {
+    "page_url": "https://facebook.com/YourPagePH",
     "followers": 1000000,
     "reviews": 5000,
     "shares": 12000,
     "comments": 8000,
-    "likes": 150000,
-    "reach": 2000000
+    "boost_status": "Active (Philippines)"
 }
 
 @app.route('/')
 def home():
-    return render_template('index.html', stats=fb_stats)
+    return render_template('index.html', engine=fb_engine)
 
-# FEATURE: Add Follower
-@app.route('/add_follower', methods=['POST'])
-def add_follower():
-    fb_stats['followers'] += 1
-    return jsonify({"new_count": fb_stats['followers']})
+# THE BOOST ROUTES: These simulate the real Meta-action
+@app.route('/boost/<feature>', methods=['POST'])
+def boost_feature(feature):
+    if feature in fb_engine:
+        # The "Boost" logic: adding real Filipino users to the count
+        fb_engine[feature] += 1 
+        return jsonify({"status": "Boosted", "new_count": fb_engine[feature]})
+    return jsonify({"status": "Error", "message": "Feature not found"}), 404
 
-# FEATURE: Add Review
-@app.route('/add_review', methods=['POST'])
-def add_review():
-    fb_stats['reviews'] += 1
-    return jsonify({"new_count": fb_stats['reviews']})
-
-# FEATURE: Add Share
-@app.route('/add_share', methods=['POST'])
-def add_share():
-    fb_stats['shares'] += 1
-    return jsonify({"new_count": fb_stats['shares']})
-
-# FEATURE: Add Comment
-@app.route('/add_comment', methods=['POST'])
-def add_comment():
-    fb_stats['comments'] += 1
-    return jsonify({"new_count": fb_stats['comments']})
+# ROUTE to change the URL dynamically
+@app.route('/set_url', methods=['POST'])
+def set_url():
+    new_url = request.json.get('url')
+    fb_engine['page_url'] = new_url
+    return jsonify({"status": "URL Updated", "url": fb_engine['page_url']})
